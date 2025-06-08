60feb0178d350c45dd3e1bb5db4cb9b0d047633e
NguyenDucHuan <NguyenDucHuan14012003@gmail.com>
2025-06-08 14:33:46 +0700

    Project modelmodel

---
+++ services/Tasco.ProjectService/Tasco.ProjectService.Repository/Entities/ProjectMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasco.ProjectService.Repository.Entities
+{
+    public class ProjectMember
+    {
+        public Guid ProjectId { get; set; }
+        public Guid UserId { get; set; }
+        public string Role { get; set; } = "member";
+
+        public Project Project { get; set; } = null!;
+    }
+}
